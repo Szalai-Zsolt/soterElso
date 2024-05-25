772b35b273873fb96464b1aacc147343e021441f
Szalai <tlosz9@gmail.com>
2024-05-25 09:17:43 +0200

    Kiiratas metodus kesz

--- src/progtetelgyak/ProgTetelGyak.java
+++ src/progtetelgyak/ProgTetelGyak.java
@@ -4,19 +4,24 @@ package progtetelgyak;
 
 public class ProgTetelGyak {
     static int[] sorozat={5,8,12,43,64};
+    static int osszeg;
    
     public static void main(String[] args) {
        
         osszegzes();
-        
+        kiir();
     }
 
    
     private static void osszegzes() {
-        int osszeg = 0;
+        osszeg = 0;
         for (int i = 0; i < sorozat.length; i++) {
             osszeg += sorozat[i];
         }
+    }
+
+    private static void kiir() {
+        System.out.printf("A tomb elemeinek osszege: %d\n", osszeg );    
     }
     
 }
